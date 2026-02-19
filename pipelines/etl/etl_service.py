cdf155fbb7780c4abb2c0c29ac9633a0e3846e0c
Giyoung Kim <giy.kim27@gmail.com>
2026-02-19 12:51:16 -0800

    Add method to create documents from dataframe

--- pipelines/etl/etl_service.py
+++ pipelines/etl/etl_service.py
@@ -9,10 +9,11 @@ class ETLService:
 
     def run(self):
         raw_df = self.ingester.load_kaggle_dataset()
-
         print(f"Loaded dataset with columns: {raw_df.columns.to_list()}")
 
-        columns = ["name", "detailed_description"]
-        preprocessed_df = self.preprocesser.select_columns(raw_df, columns)
+        description_columns = ["name", "detailed_description", "genres", "tags"]
+        stats_columns = ["name", "price", "metacritic_score", "average_playtime_forever"]
+        documents = self.preprocesser.build_documents(raw_df, [description_columns, stats_columns])
 
-        print(f"Preprocessed dataset: {preprocessed_df.columns.to_list()}")
+        print(f"Created documents.")
+        print(f"{not documents is None}")
--- pipelines/etl/preprocess.py
+++ pipelines/etl/preprocess.py
@@ -2,12 +2,43 @@ import pandas as pd
 
 
 class DataPreprocesser:
+    NAME_COLUMN = "name"
+
     def __init__(self):
         pass
 
-    def select_columns(self, raw_df: pd.DataFrame, columns: list) -> pd.DataFrame:
-        df = raw_df.copy()
+    def build_documents(self, df: pd.DataFrame, columns_groups: list[list[str]]) -> list[dict]:
+        documents = []
 
-        df = df[columns]
+        for columns in columns_groups:
+            if self.NAME_COLUMN not in columns:
+                print(f"Columns {self.NAME_COLUMN} is a required column. Skipping creating documents for {columns}.")
+                continue
 
-        return df
+            group_df = self._select_columns(df, columns)
+            group_df = self._remove_na(group_df)
+
+            for _, row in group_df.iterrows():
+                documents.append({
+                    "name": row["name"],
+                    "content": self._build_content(row),
+                })
+
+        return documents
+
+    def _select_columns(self, df: pd.DataFrame, columns: list[str]) -> pd.DataFrame:
+        df = df.copy()
+
+        return df[columns]
+    
+    def _remove_na(self, df: pd.DataFrame) -> pd.DataFrame:
+        df = df.copy()
+
+        return df.dropna()
+    
+    def _build_content(self, row: pd.Series) -> str:
+        return "\n".join(
+            f"{col.replace('_', '').title()}: {row[col]}"
+            for col in row.index
+            if not col == "name"
+        )
